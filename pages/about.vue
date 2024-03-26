43115690bd489b0e702b176d75d9b22840cdcea9
09step <92272272+09step@users.noreply.github.com>
2024-03-26 18:37:59 +0100

    improving performance with preloading threejs files

--- pages/about.vue
+++ pages/about.vue
@@ -1,40 +1,36 @@
 <template>
-        <Head>
-            <Title>About</Title>
-        </Head>
     <div class="flex flex-col lg:flex-row justify-center inset-0 lg:left-48 px-4 absolute">
-        <div class="lg:my-auto">
-            <ThreeNew />
-        </div>
-        <div class="w-16"></div>
-        <div class="flex flex-col w-full md:w-1/2 lg:w-1/4  my-auto md:inset-0">
-            <p class="text-justify md:justify-center">I am currently studying interaction design at the Zurich University of the Arts in 
-                Switzerland. Looking to further my understanding of service and product design while continuing to explore 
-                interdisciplinary learning. Feel free to contact me under:
-            </p>
-            <p>
-                stepan@vedunov.com
-            </p>
-        </div>
+      <div class="lg:my-auto">
+        <ThreeNew />
+      </div>
+      <div class="w-16"></div>
+      <div class="flex flex-col w-full md:w-1/2 lg:w-1/4 my-auto md:inset-0">
+        <p class="text-justify md:justify-center">I am currently studying interaction design at the Zurich University of the Arts in Switzerland. Looking to further my understanding of service and product design while continuing to explore interdisciplinary learning. Feel free to contact me under: </p>
+        <p>stepan@vedunov.com</p>
+      </div>
     </div>
-</template>
-
-<script setup>
-
-</script>
-
-<style lang="scss" scoped> 
-    h2 {
-        margin-bottom: 20px;
-        font-size: 36px;
-    }
-    
-    p {
-        margin: 20px 0;
-    }
-
-    // .router-link-exact-active {
-    //     color:rgb(122, 217, 26);
-    // } 
-
-</style>
+  </template>
+  
+  <script setup>
+  import { onMounted } from 'vue';
+  
+  onMounted(() => {
+    // Preload Three.js script
+    const threeScript = document.createElement('script');
+    threeScript.src = 'ThreeNew.vue';
+    threeScript.defer = true;
+    document.head.appendChild(threeScript);
+  });
+  </script>
+  
+  <style lang="scss" scoped>
+  h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+  }
+  
+  p {
+    margin: 20px 0;
+  }
+  </style>
+  
