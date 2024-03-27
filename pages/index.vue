bf10e0b9a115f6c282f85667a34554bc4a37f589
09step <92272272+09step@users.noreply.github.com>
2024-03-27 10:56:59 +0100

    dynamic loading of three files

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,12 @@
         <Head>
             <Title>Home</Title>
         </Head>
-        <div>
-            <LandingAnimation />
+        <div v-if="isWebsiteLoaded">
+            <LandingAnimation v-if="!landingAnimationStarted" @animationComplete="handleAnimationComplete" />
         </div>
+        <!-- <div>
+            <div ref="landingAnimationContainer"></div>
+        </div> -->
         <div class="flex inset-0 absolute lg:left-48">
             <div class=" hidden lg:flex flex-col justify-center w-custom-50">
                 <img v-if="hoveredProject" :src="hoveredProject ? hoveredProject.thumb : project.thumb" alt="project img" loading="lazy" class="object-cover min-w-full aspect-[4/3]">
@@ -19,9 +22,18 @@
 </template>
 
 <script setup>
+    // import LandingAnimation from '../LazyThree.vue';
     import jsonData from './assets/projects/projects.json';
+    import LandingAnimation from '../components/LandingAnimation.vue';
     const project = jsonData;
     const hoveredProject = ref(null);
+
+    const isWebsiteLoaded = ref(false);
+    const landingAnimationStarted = ref(false);
+
+    const handleAnimationComplete = () => {
+        landingAnimationStarted.value = true;
+    };
 
     // const largeBreakpoint = window.matchMedia("(min-width: 700px)")
 
@@ -33,16 +45,32 @@
         hoveredProject.value = null;
     };
 
+
     onMounted(() => {
-        
-        project.forEach(p => {
-            const link = document.createElement('link');
-            link.rel = 'preload';
-            link.as = 'image';
-            link.href = p.thumb;
-            document.head.appendChild(link);
-        });
-    });
+  // Simulate website loading completion after a delay
+  setTimeout(() => {
+    isWebsiteLoaded.value = true;
+    preloadImages();
+  }, 2000); // Adjust the delay as needed
+});
+
+const preloadImages = () => {
+  project.forEach(p => {
+    const img = new Image();
+    img.src = p.thumb;
+  });
+};
+
+// Preload images using link preload with appropriate 'as' attribute
+project.forEach(p => {
+  const link = document.createElement('link');
+  link.rel = 'preload';
+  link.as = 'image';
+  link.href = p.thumb;
+  document.head.appendChild(link);
+});
+
+
     
 </script>
 
