75680e209e3a76760a4f4eabbe5a2ddff3119409
09step <92272272+09step@users.noreply.github.com>
2024-04-07 14:38:00 +0200

    Change order

--- components/ThreeNew.vue
+++ components/ThreeNew.vue
@@ -13,35 +13,37 @@ let rotationIncrement = 0.0003;
 let rotationDirection = 1;
 let lastRenderTime = 0;
 const fpsInterval = 100;
+const width = 600;
+const height = 600;
 
 onMounted(() => {
-  window.addEventListener('resize', reactiveWindow);
+  // window.addEventListener('resize', reactiveWindow);
   init();
   loadModel();
   animate();
 });
 
-onUnmounted(() => {
-  window.removeEventListener('resize', reactiveWindow);
-});
+// onUnmounted(() => {
+//   window.removeEventListener('resize', reactiveWindow);
+// });
 
-function reactiveWindow() {
-  const isMobile = window.innerWidth <= 600;
-  const maxWidth = isMobile ? 400 : 600; // Change maximum width for mobile
-  const width = maxWidth;
-  const height = isMobile ? 400 : window.innerHeight - 200; // Change height for mobile
+// function reactiveWindow() {
+//   const isMobile = window.innerWidth <= 600;
+//   const maxWidth = isMobile ? 400 : 600; // Change maximum width for mobile
+//   const width = maxWidth;
+//   const height = isMobile ? 400 : window.innerHeight - 200; // Change height for mobile
 
-  renderer.setSize(width, height);
-  camera.aspect = width / height;
-  camera.updateProjectionMatrix();
-}
+//   renderer.setSize(width, height);
+//   camera.aspect = width / height;
+//   camera.updateProjectionMatrix();
+// }
 
 
 function init() {
-  const isMobile = window.innerWidth <= 600;
-  const maxWidth = isMobile ? 400 : 600; // Change maximum width for mobile
-  const width = maxWidth;
-  const height = isMobile ? 400 : window.innerHeight - 200; // Change height for mobile
+  // const isMobile = window.innerWidth <= 600;
+  // const maxWidth = isMobile ? 400 : 600; // Change maximum width for mobile
+  // const width = maxWidth;
+  // const height = isMobile ? 400 : window.innerHeight - 200; // Change height for mobile
 
   camera = new THREE.PerspectiveCamera(30, width / height, 0.1, 2);
   camera.position.y = 1.2;
@@ -55,7 +57,7 @@ function init() {
 
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(width, height);
-  effect = new AsciiEffect(renderer, '    ....::', { invert: true, resolution: 0.13 });
+  effect = new AsciiEffect(renderer, '    ....::', { invert: true, resolution: 0.15 });
   effect.setSize(width, height);
   effect.domElement.style.color = '#006CFF';
 
