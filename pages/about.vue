dde956990a5d1a9aa933029b78f0a1c3c609d8e0
09step <92272272+09step@users.noreply.github.com>
2024-04-14 14:45:44 +0200

    mobile about section

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,5 @@
 <template>
-    <div class="flex flex-col lg:flex-row justify-center inset-0 lg:left-48 px-4 absolute overflow-hidden">
-      <div class="md:h-32 lg:hidden"></div>
+    <div class="flex flex-col lg:flex-row justify-center inset-0 lg:left-48 px-4 lg:absolute overflow-hidden">
       <transition name="opacity-p5">
         <div v-if="showThree" class="lg:my-auto max-md:scale-50 max-md:flex max-md:justify-center max-md:h-96 max-md:-translate-y-16">
             <ThreeNew />
@@ -12,7 +11,7 @@
       </div>
       <transition name="fade-titles">
         <div v-if="showTitles" class="flex flex-col w-full md:w-1/2 lg:w-1/4 md:my-auto md:inset-0">
-          <p lang="en" class="text-justify md:justify-center">I am an interaction designer looking to further my understanding in the intersection of digital and physical. Feel free to contact me under: </p>
+          <p lang="en" class="text-justify md:justify-center margin-handeler">I am an interaction designer looking to further my understanding in the intersection of digital and physical. Feel free to contact me under: </p>
           <p>stepan@vedunov.com</p>
         </div>
       </transition>
@@ -42,6 +41,12 @@
   h2 {
     margin-bottom: 20px;
     font-size: 36px;
+  }
+
+  @media (max-width: 700px){
+    .margin-handeler{
+      margin: 0;
+    }
   }
   
   p {
